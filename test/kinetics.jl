b2fa91b4280b7e13cc1b1569be657e9aa9fb9ad0
Steffen Maass <maassstef@gmail.com>
2023-08-18 17:42:20 +0200

    some updates

--- test/kinetics.jl
+++ test/kinetics.jl
@@ -10,22 +10,54 @@ begin
 	_Pkg.activate(joinpath(@__DIR__, ".."))
 	using PyCall
 	using DataFrames
+	using LessUnitful
 end
+
+# ╔═╡ 44a506d1-b0e8-4bb0-8265-fac97271efff
+md"""
+## Units
+"""
+
+# ╔═╡ 3afa3da7-d14b-43a9-bd43-c0ce0550086f
+begin
+	@unitfactors cm eV μF μA
+	@phconstants c_0
+end;
+
+# ╔═╡ 1dc9ffb6-02e2-4278-99c8-d1495818256a
+md"""
+## Data
+"""
+
+# ╔═╡ b24253cc-e044-48ad-8d1c-f12a6765c398
+begin
+	const ϕ_we 	= -0.5
+	const T 	= 298.0
+	const C_gap = 20 * μF/cm^2
+	const ϕ_pzc = 0.16 * ufac"V"
+	const local_pH = 6.8
+end;
 
 # ╔═╡ ba894fa1-fba8-40db-9459-7ec78462a246
 md"""
-# CatMAP
+## CatMAP
 """
 
 # ╔═╡ 1f020e47-e4cb-4202-80f7-39421e4ce471
-function instantiate_catmap_template(template_file_path, ϕ_we)
+function instantiate_catmap_template(template_file_path, ϕ_we, ϕ, local_pH)
     
     input_instance_string = open(template_file_path, "r") do template_file
         read(template_file, String)
     end
 
-    # replace descriptor range
-    input_instance_string = replace(input_instance_string, r"descriptor_ranges.*" => "descriptor_ranges = [[$ϕ_we, $ϕ_we], [298, 298]]")
+	replacements = Dict(
+		r"descriptor_ranges.?=.*" =>"descriptor_ranges = [[$ϕ_we, $ϕ_we], [$T, $T]]",
+		r"voltage_diff_drop.?=.*" => "voltage_diff_drop = $ϕ",
+		r"pH.?=.*" => "pH = $local_pH",
+		r"Upzc.?=.*" => "Upzc = $ϕ_pzc",
+		r"\nsigma_input.?=.*" => "\nsigma_input = $((ϕ_we - ϕ - ϕ_pzc) * C_gap/(μF/cm^2))",
+	)
+    input_instance_string = replace(input_instance_string, replacements...)
 
     input_instance_file_name = "CO2R.mkm"
     open(input_instance_file_name, "w") do input_instance_file
@@ -41,22 +73,12 @@ begin
 	isfile(template_file_path)
 end
 
-# ╔═╡ 9ae881ba-2ba1-466d-b892-98d6f75f133b
-ϕ_we = -0.5;
-
-# ╔═╡ 50b4d059-6913-451d-93dd-f4d591792319
-begin
-	catmap_instance_path = instantiate_catmap_template(template_file_path, ϕ_we)
-	catmap_instance_path = abspath(joinpath(".", catmap_instance_path))
-	isfile(catmap_instance_path)
-end
-
 # ╔═╡ 6120ec04-80c8-423c-a92a-44583ba9bac7
 catmap_df = let
 py"""
 from catmap import ReactionModel
 
-def catmap_kinetic_model(setup_file, Stern_capacitance=20.0):
+def catmap_kinetic_model(setup_file):
 
 	# ReactionModel is the main class that is initialized with a setup-file
 	model = ReactionModel(setup_file=setup_file)
@@ -112,32 +134,36 @@ def catmap_kinetic_model(setup_file, Stern_capacitance=20.0):
 			descriptor_range[idx] for idx, descriptor_range in zip(get_idx_tuple(i, model.resolution), descriptor_ranges)
 		]
 		descriptor_grid[i] = descriptor_values
-		### create dictionary with different values of sigma
-		if 'voltage' in model.descriptor_names:
-			rxn_params_grid[i] = {}
-			voltage = descriptor_values[model.descriptor_names.index('voltage')] 
-			voltage_reduced  = voltage - model.extrapolated_potential
-			voltage_diff_drops = linspace(0.0, voltage_reduced, 100) if (voltage_reduced > 0) else linspace(voltage_reduced, 0.0, 100)
-			sigmas = [
-				(voltage_reduced - voltage_diff_drop) * Stern_capacitance for voltage_diff_drop in voltage_diff_drops
-			]
-			for sigma in sigmas:
-				model.sigma_input = sigma
-				rxn_params_grid[i][sigma] = model.scaler.get_rxn_parameters(descriptor_values)
-		else:
-			rxn_params_grid[i] = model.scaler.get_rxn_parameters(descriptor_values)
-
-	return rxn_params_grid, model.solver._gas_energies, model.solver._site_energies, model.adsorbate_names + model.transition_state_names, model.gas_names, model.site_names
+		rxn_params_grid[i] = {
+			k:v for k, v in zip(
+				model.adsorbate_names + model.transition_state_names,
+				model.scaler.get_rxn_parameters(descriptor_values)
+			)
+		}
+		for k, v in zip(model.gas_names, model.solver._gas_energies):
+			rxn_params_grid[i][k] = v
+		for k, v in zip(model.site_names, model.solver._site_energies):
+			rxn_params_grid[i][k] = v
+	return rxn_params_grid[0], model.species_definitions
 """
-	param_grid, gas_energies, site_energies, surface_species, bulk_species, surfaces = py"catmap_kinetic_model"(catmap_instance_path, 1)
-
-	sigmas = convert.(Float64, collect(keys(param_grid[1])))
-	energies_list = collect(values(param_grid[1]))
-	I = sortperm(sigmas)
-	sigmas .= sigmas[I]
-	rows = stack([[sigma, append!(energies, gas_energies, site_energies)...] for (sigma, energies) in zip(sigmas, energies_list)], dims=2)
-
-	DataFrame(permutedims(rows, (2,1)), ["σ", surface_species..., bulk_species..., surfaces...])
+	df = nothing
+	for ϕ in range(ϕ_we, 0.0, 11)
+		σ = (ϕ_we - ϕ - ϕ_pzc) * C_gap
+		params = Dict("ϕ_we" => ϕ_we, "ϕ" => ϕ, "σ" => σ)
+		
+		catmap_instance_path = instantiate_catmap_template(template_file_path, ϕ_we, ϕ, local_pH)
+		catmap_instance_path = abspath(joinpath(".", catmap_instance_path))
+		energies, specd = py"catmap_kinetic_model"(catmap_instance_path)
+		energies = convert(Dict{String, Float64}, energies)
+		
+		if isnothing(df)
+			@show specd
+			df = DataFrame(params..., energies...)
+		else
+			push!(df, merge(params, energies))
+		end
+	end
+	df
 end
 
 # ╔═╡ 8a87380d-20e6-4c27-950b-25003b98bf30
@@ -146,7 +172,8 @@ md"""
 """
 
 # ╔═╡ 30c7d656-e197-495e-bd71-9c71214d1857
-py"""
+begin
+	py"""
 from ase.thermochemistry import HarmonicThermo, IdealGasThermo
 from ase.build import molecule
 
@@ -163,12 +190,15 @@ def get_thermal_correction_ideal_gas(T, frequencies, symmetrynumber, geometry, s
 
     free_energy = H-T*S
     return free_energy
-"""
+	"""
+	get_thermal_correction_adsorbate = py"get_thermal_correction_adsorbate"
+	get_thermal_correction_ideal_gas = py"get_thermal_correction_ideal_gas"
+end;
 
 # ╔═╡ f4e3b67b-ee51-4648-b780-c563557e7ca6
 begin
 	bulk_species        = ["H2_g", "CO2_g", "CO_g", "OH_g", "H2O_g", "ele_g"]
-	surface_species     = ["CO2*_t", "COOH*_t", "CO*_t"]
+	surface_species     = ["CO2_t", "COOH_t", "CO_t"]
 	transition_state    = ["COOH-H2O-ele_t"]
 end;
 
@@ -180,10 +210,10 @@ md"""
 # ╔═╡ fdbc2d16-b26a-4df2-b6e9-a6fa1a825be2
 E_f = Dict( 
 	"CO2_g" => 0.0 * eV,
-	"CO2*_t" => 0.657600203 * eV,
-	"COOH*_t" => 0.128214079 * eV,
+	"CO2_t" => 0.657600203 * eV,
+	"COOH_t" => 0.128214079 * eV,
 	"COOH-H2O-ele_t" => 0.95 * eV,
-	"CO*_t" => -0.02145440850567823 * eV,
+	"CO_t" => -0.02145440850567823 * eV,
 	"CO_g" => 0.2701852315 * eV,
 	"ele_g" => 0.0 * eV,
 	"OH_g" => 0.0 * eV,
@@ -199,10 +229,10 @@ md"""
 # ╔═╡ c2640ab8-8926-4872-abdf-adc509f56d59
 frequencies = Dict(
 	        "CO2_g" => [24.1, 70.7, 635.8, 640.5, 1312.2, 2361.2] * ufac"h / cm / eV" * c_0,
-	        "CO2*_t" => [136.85, 183.6, 212.95, 250.7, 306.0, 510.55, 562.25, 1176.05, 1889.85] * ufac"h / cm / eV" * c_0,
-	        "COOH*_t" => [102.25, 172.05, 242.65, 265.55, 303.45, 520.8, 646.8, 794.1500000000001, 1032.6999999999998, 1344.35, 1658.15, 3551.35] * ufac"h  / cm / eV" * c_0,
+	        "CO2_t" => [136.85, 183.6, 212.95, 250.7, 306.0, 510.55, 562.25, 1176.05, 1889.85] * ufac"h / cm / eV" * c_0,
+	        "COOH_t" => [102.25, 172.05, 242.65, 265.55, 303.45, 520.8, 646.8, 794.1500000000001, 1032.6999999999998, 1344.35, 1658.15, 3551.35] * ufac"h  / cm / eV" * c_0,
 	        "COOH-H2O-ele_t" => [],
-	        "CO*_t" => [129.65, 155.55, 189.8, 227.5, 2073.25] * ufac"h / cm / eV" * c_0,
+	        "CO_t" => [129.65, 155.55, 189.8, 227.5, 2073.25] * ufac"h / cm / eV" * c_0,
 	        "CO_g" => [89.8, 127.2, 2145.5] * ufac"h / cm / eV" * c_0,
 	        "ele_g" => [],
 	        "OH_g" => [],
@@ -237,7 +267,7 @@ ideal_gas_params = Dict(
 ### ideal gases
 for sp in bulk_species
 	if sp ∉ ["OH_g", "ele_g"]
-		thermo_corrections[sp] += py"get_thermal_correction_ideal_gas"(T, frequencies[sp], ideal_gas_params[sp]...) * eV
+		thermo_corrections[sp] += get_thermal_correction_ideal_gas(T, frequencies[sp], ideal_gas_params[sp]...) * eV
 	end
 end
 
@@ -249,7 +279,7 @@ md"""
 # ╔═╡ 49808249-8a31-475b-9a04-2630545bf4d4
 ### harmonic adsorbates
 for sp ∈ surface_species
-	thermo_corrections[sp] += py"get_thermal_correction_adsorbate"(T, frequencies[sp]) * eV
+	thermo_corrections[sp] += get_thermal_correction_adsorbate(T, frequencies[sp]) * eV
 	#println("$sp: $(E_f[sp] / eV)")
 end;
 
@@ -259,7 +289,7 @@ md"""
 """
 
 # ╔═╡ dd958e98-aab7-489c-a6fc-6261d85fbea4
-thermo_corrections["COOH-H2O-ele_t"] += (thermo_corrections["COOH*_t"] + thermo_corrections["CO*_t"]) / 2;
+thermo_corrections["COOH-H2O-ele_t"] += (thermo_corrections["COOH_t"] + thermo_corrections["CO_t"]) / 2;
 
 # ╔═╡ 58bd5959-c4c9-4351-8852-063743b7a937
 md"""
@@ -276,17 +306,11 @@ $σ(U) = C_{gap} (ϕ_{we} - ϕ_{pzc} - ϕ^\ddagger)$
 where the gap capacitance between the working electrode and the reaction plane ($\ddagger$) is given by $C_{gap} = 20~μF/cm^2$. The potential of zero current is measured to be $ϕ_{pzc} = 0.16~V$.
 """
 
-# ╔═╡ b2eb9a0d-cb44-492f-9297-26833ba41354
-begin
-	C_gap = 20 * μF/cm^2
-	ϕ_pzc = 0.16 * ufac"V"
-end;
-
 # ╔═╡ 18966e29-62c4-4e0e-8b96-47ac0e026dc2
 electro_correction_params = Dict(
-	"CO2*_t"  => [-0.000286600929 / (μA/cm^2)^2, 0.0297720125 / (μA/cm^2)],
-	"COOH*_t" => [-9.0295682e-05 / (μA/cm^2)^2, 0.00226896383 / (μA/cm^2)],
-	"CO*_t"   => [-0.000189106972 / (μA/cm^2)^2,-0.00942574086 / (μA/cm^2)]
+	"CO2_t"  => [-0.000286600929 / (μA/cm^2)^2, 0.0297720125 / (μA/cm^2)],
+	"COOH_t" => [-9.0295682e-05 / (μA/cm^2)^2, 0.00226896383 / (μA/cm^2)],
+	"CO_t"   => [-0.000189106972 / (μA/cm^2)^2,-0.00942574086 / (μA/cm^2)]
 );
 
 # ╔═╡ 77b9a080-7a3b-4835-9d69-afa13eff3757
@@ -295,13 +319,13 @@ md"""
 """
 
 # ╔═╡ 9c02ef80-80ca-4b33-84f6-8cba62527539
-function compute_rateconstants_explicit!(σ, ϕ_we, ϕ, local_pH)
+function compute_energies_explicit!(G_f, σ, ϕ_we, ϕ, local_pH)
 	β = 0.5 # hack
 	
-	Tval = eltype(kf)
+	
 	
 	# compute corrections due to surface charge densities
-	electro_corrections = Dict(zip(keys(E_f), zeros(Tval, length(E_f))))
+	electro_corrections = Dict(zip(keys(E_f), zeros(Float64, length(E_f))))
 
 	## simple_electrochemical
 	#electro_corrections["ele_g"] -= (ϕ_we - (ϕ_we - u[iϕ])) * eV
@@ -312,9 +336,9 @@ function compute_rateconstants_explicit!(σ, ϕ_we, ϕ, local_pH)
 
 
 	## hbond_electrochemical
-	electro_corrections["COOH*_t"] += -0.25 * eV
-	electro_corrections["CO2*_t"] += 0.0 * eV
-	electro_corrections["CO*_t"] += -0.1 * eV
+	electro_corrections["COOH_t"] += -0.25 * eV
+	electro_corrections["CO2_t"] += 0.0 * eV
+	electro_corrections["CO_t"] += -0.1 * eV
 
 	## hbond_surface_charge_density
 	for sp in surface_species
@@ -326,27 +350,48 @@ function compute_rateconstants_explicit!(σ, ϕ_we, ϕ, local_pH)
 	G_H2        = E_f["H2_g"] + thermo_corrections["H2_g"]
 	G_H         = 0.5 * G_H2 - .0592 * local_pH / 298.14 * T * eV
 	G_OH        = G_H2O - G_H
-
-
-	G_f = Dict(zip([bulk_species; surface_species; transition_state], zeros(Tval, nc + na + length(transition_state))))
 	
 	for sp in [bulk_species; surface_species; transition_state]
 		G_f[sp] += E_f[sp] + thermo_corrections[sp] + electro_corrections[sp]
 	end
 	G_f["OH_g"] += G_OH
-
-	return G_f
+	nothing
 end;
+
+# ╔═╡ 90d20692-ce49-419e-8377-9e2b72d176a6
+explicit_df = let
+	ns = names(catmap_df)
+	df = DataFrame(fill(Float64[], (length(ns),)), ns)
+	
+	for ϕ in range(ϕ_we, 0.0, 11)
+		G_f = Dict(zip([bulk_species; surface_species; transition_state], zeros(length(E_f))))
+		σ = (ϕ_we - ϕ - ϕ_pzc) * C_gap
+		params = Dict("ϕ" => ϕ, "ϕ_we" => ϕ_we, "σ" => σ)
+		compute_energies_explicit!(G_f, σ, ϕ_we, ϕ, local_pH)
+		G_f["g"] = 0.0
+		G_f["t"] = 0.0
+		for (k, v) in pairs(G_f)
+			G_f[k] = v / eV
+		end
+		push!(df, merge(params, G_f))
+	end
+	df
+end
+
+# ╔═╡ 3215092e-a913-4c73-ac01-fc725beccb05
+catmap_df .- explicit_df
 
 # ╔═╡ 00000000-0000-0000-0000-000000000001
 PLUTO_PROJECT_TOML_CONTENTS = """
 [deps]
 DataFrames = "a93c6f00-e57d-5684-b7b6-d8193f3e46c0"
+LessUnitful = "f29f6376-6e90-4d80-80c9-fb8ec61203d5"
 Pkg = "44cfe95a-1eb2-52ea-b672-e2afdf69b78f"
 PyCall = "438e738f-606a-5dbb-bf0a-cddfbfd45ab0"
 
 [compat]
 DataFrames = "~1.6.1"
+LessUnitful = "~0.6.1"
 PyCall = "~1.96.1"
 """
 
@@ -356,7 +401,7 @@ PLUTO_MANIFEST_TOML_CONTENTS = """
 
 julia_version = "1.10.0-beta1"
 manifest_format = "2.0"
-project_hash = "0b7a9345df9d233cd1b35cff42236b950960669c"
+project_hash = "33e2c182cb2ae1d2c697d94218cd0571206bd9f4"
 
 [[deps.ArgTools]]
 uuid = "0dad84c5-d112-42e6-8d28-ef12dabb789f"
@@ -367,6 +412,23 @@ uuid = "56f22d72-fd6d-98f1-02f0-08ddc0907c33"
 
 [[deps.Base64]]
 uuid = "2a0f44e3-6c83-55bd-87e4-b1978d98bd5f"
+
+[[deps.Calculus]]
+deps = ["LinearAlgebra"]
+git-tree-sha1 = "f641eb0a4f00c343bbc32346e1217b86f3ce9dad"
+uuid = "49dc2e85-a5d0-5ad3-a950-438e2897f1b9"
+version = "0.5.1"
+
+[[deps.ChainRulesCore]]
+deps = ["Compat", "LinearAlgebra", "SparseArrays"]
+git-tree-sha1 = "e30f2f4e20f7f186dc36529910beaedc60cfa644"
+uuid = "d360d2e6-b24c-11e9-a2a3-2a2ae2dbcce4"
+version = "1.16.0"
+
+[[deps.CommonSolve]]
+git-tree-sha1 = "0eee5eb66b1cf62cd6ad1b460238e60e4b09400c"
+uuid = "38540f10-b2f7-11e9-35d8-d573e4eb0ff2"
+version = "0.2.4"
 
 [[deps.Compat]]
 deps = ["UUIDs"]
@@ -388,6 +450,20 @@ deps = ["Downloads", "JSON", "VersionParsing"]
 git-tree-sha1 = "8c86e48c0db1564a1d49548d3515ced5d604c408"
 uuid = "8f4d0f93-b110-5947-807f-2305c1781a2d"
 version = "1.9.1"
+
+[[deps.ConstructionBase]]
+deps = ["LinearAlgebra"]
+git-tree-sha1 = "fe2838a593b5f776e1597e086dcd47560d94e816"
+uuid = "187b0558-2788-49d3-abe0-74a17ed4e7c9"
+version = "1.5.3"
+
+    [deps.ConstructionBase.extensions]
+    ConstructionBaseIntervalSetsExt = "IntervalSets"
+    ConstructionBaseStaticArraysExt = "StaticArrays"
+
+    [deps.ConstructionBase.weakdeps]
+    IntervalSets = "8197267c-284f-5f27-9208-e0e47529a953"
+    StaticArrays = "90137ffa-7385-5640-81b9-e52037218182"
 
 [[deps.Crayons]]
 git-tree-sha1 = "249fe38abf76d48563e2f4556bebd215aa317e15"
@@ -463,6 +539,12 @@ git-tree-sha1 = "f2355693d6778a178ade15952b7ac47a4ff97996"
 uuid = "b964fa9f-0449-5b57-a5c2-d3ea65f4040f"
 version = "1.3.0"
 
+[[deps.LessUnitful]]
+deps = ["PhysicalConstants", "Unitful"]
+git-tree-sha1 = "7d7b92d12f4748bc4ed196414f04499fb141a365"
+uuid = "f29f6376-6e90-4d80-80c9-fb8ec61203d5"
+version = "0.6.1"
+
 [[deps.LibCURL]]
 deps = ["LibCURL_jll", "MozillaCACerts_jll"]
 uuid = "b27032c2-a3e7-50c8-80cd-2d36dbcbfd21"
@@ -507,6 +589,12 @@ deps = ["Artifacts", "Libdl"]
 uuid = "c8ffd9c3-330d-5841-b78e-0817d7145fa1"
 version = "2.28.2+0"
 
+[[deps.Measurements]]
+deps = ["Calculus", "LinearAlgebra", "Printf", "RecipesBase", "Requires"]
+git-tree-sha1 = "51d946d38d62709d6a2d37ea9bcc30c80c686801"
+uuid = "eff96d63-e80a-5855-80a2-b1b0885c5ab7"
+version = "2.9.0"
+
 [[deps.Missings]]
 deps = ["DataAPI"]
 git-tree-sha1 = "f66bdc5de519e8f8ae43bdc598782d35a25b1272"
@@ -539,6 +627,12 @@ deps = ["Dates", "PrecompileTools", "UUIDs"]
 git-tree-sha1 = "716e24b21538abc91f6205fd1d8363f39b442851"
 uuid = "69de0a69-1ddd-5017-9359-2bf0b02dc9f0"
 version = "2.7.2"
+
+[[deps.PhysicalConstants]]
+deps = ["Measurements", "Roots", "Unitful"]
+git-tree-sha1 = "cd4da9d1890bc2204b08fe95ebafa55e9366ae4e"
+uuid = "5ad8b20f-a522-5ce9-bfc9-ddf1d5bda6ab"
+version = "0.2.3"
 
 [[deps.Pkg]]
 deps = ["Artifacts", "Dates", "Downloads", "FileWatching", "LibGit2", "Libdl", "Logging", "Markdown", "Printf", "REPL", "Random", "SHA", "Serialization", "TOML", "Tar", "UUIDs", "p7zip_jll"]
@@ -587,10 +681,38 @@ uuid = "3fa0cd96-eef1-5676-8a61-b3b8758bbffb"
 deps = ["SHA"]
 uuid = "9a3f8284-a2c9-5f02-9a11-845980a1fd5c"
 
+[[deps.RecipesBase]]
+deps = ["PrecompileTools"]
+git-tree-sha1 = "5c3d09cc4f31f5fc6af001c250bf1278733100ff"
+uuid = "3cdcf5f2-1ef4-517c-9805-6587b60abb01"
+version = "1.3.4"
+
 [[deps.Reexport]]
 git-tree-sha1 = "45e428421666073eab6f2da5c9d310d99bb12f9b"
 uuid = "189a3867-3050-52da-a836-e630ba90ab69"
 version = "1.2.2"
+
+[[deps.Requires]]
+deps = ["UUIDs"]
+git-tree-sha1 = "838a3a4188e2ded87a4f9f184b4b0d78a1e91cb7"
+uuid = "ae029012-a4dd-5104-9daa-d747884805df"
+version = "1.3.0"
+
+[[deps.Roots]]
+deps = ["ChainRulesCore", "CommonSolve", "Printf", "Setfield"]
+git-tree-sha1 = "ff42754a57bb0d6dcfe302fd0d4272853190421f"
+uuid = "f2b01f46-fcfa-551c-844a-d8ac1e96c665"
+version = "2.0.19"
+
+    [deps.Roots.extensions]
+    RootsForwardDiffExt = "ForwardDiff"
+    RootsIntervalRootFindingExt = "IntervalRootFinding"
+    RootsSymPyExt = "SymPy"
+
+    [deps.Roots.weakdeps]
+    ForwardDiff = "f6369f11-7733-5829-9624-2563aa707210"
+    IntervalRootFinding = "d2bf35a9-74e0-55ec-b149-d360ff49b807"
+    SymPy = "24249f21-da20-56a4-8eb1-6a02cf4ae2e6"
 
 [[deps.SHA]]
 uuid = "ea8e919c-243c-51af-8825-aaa63cd721ce"
@@ -605,6 +727,12 @@ version = "1.4.0"
 [[deps.Serialization]]
 uuid = "9e88b42a-f829-5b0c-bbe9-9e923198166b"
 
+[[deps.Setfield]]
+deps = ["ConstructionBase", "Future", "MacroTools", "StaticArraysCore"]
+git-tree-sha1 = "e2cc6d8c88613c05e1defb55170bf5ff211fbeac"
+uuid = "efcf1570-3423-57d1-acb7-fd33fddbac46"
+version = "1.1.1"
+
 [[deps.Sockets]]
 uuid = "6462fe0b-24de-5631-8697-dd941f90decc"
 
@@ -618,6 +746,11 @@ version = "1.1.1"
 deps = ["Libdl", "LinearAlgebra", "Random", "Serialization", "SuiteSparse_jll"]
 uuid = "2f01184e-e22b-5df5-ae63-d93ebab69eaf"
 version = "1.10.0"
+
+[[deps.StaticArraysCore]]
+git-tree-sha1 = "36b3d696ce6366023a0ea192b4cd442268995a0d"
+uuid = "1e83bf80-4336-4d27-bf5d-d5a4f845583c"
+version = "1.4.2"
 
 [[deps.Statistics]]
 deps = ["LinearAlgebra", "SparseArrays"]
@@ -667,6 +800,20 @@ uuid = "cf7118a7-6976-5b1a-9a39-7adc72f591a4"
 [[deps.Unicode]]
 uuid = "4ec0a83e-493e-50e2-b9ac-8f72acf5a8f5"
 
+[[deps.Unitful]]
+deps = ["Dates", "LinearAlgebra", "Random"]
+git-tree-sha1 = "607c142139151faa591b5e80d8055a15e487095b"
+uuid = "1986cc42-f94f-5a68-af5c-568840ba703d"
+version = "1.16.3"
+
+    [deps.Unitful.extensions]
+    ConstructionBaseUnitfulExt = "ConstructionBase"
+    InverseFunctionsUnitfulExt = "InverseFunctions"
+
+    [deps.Unitful.weakdeps]
+    ConstructionBase = "187b0558-2788-49d3-abe0-74a17ed4e7c9"
+    InverseFunctions = "3587e190-3f89-42d0-90ee-14403ec27112"
+
 [[deps.VersionParsing]]
 git-tree-sha1 = "58d6e80b4ee071f5efd07fda82cb9fbe17200868"
 uuid = "81def892-9a0e-5fdd-b105-ffc91e053289"
@@ -695,32 +842,35 @@ version = "17.4.0+0"
 
 # ╔═╡ Cell order:
 # ╠═20f09fe8-3d2f-11ee-3749-550c6dbe9f0e
+# ╟─44a506d1-b0e8-4bb0-8265-fac97271efff
+# ╠═3afa3da7-d14b-43a9-bd43-c0ce0550086f
+# ╟─1dc9ffb6-02e2-4278-99c8-d1495818256a
+# ╠═b24253cc-e044-48ad-8d1c-f12a6765c398
 # ╟─ba894fa1-fba8-40db-9459-7ec78462a246
-# ╟─1f020e47-e4cb-4202-80f7-39421e4ce471
+# ╠═1f020e47-e4cb-4202-80f7-39421e4ce471
 # ╠═9d4a2505-9c24-4ed2-bf02-f6598f296f11
-# ╠═9ae881ba-2ba1-466d-b892-98d6f75f133b
-# ╠═50b4d059-6913-451d-93dd-f4d591792319
-# ╟─6120ec04-80c8-423c-a92a-44583ba9bac7
+# ╠═6120ec04-80c8-423c-a92a-44583ba9bac7
 # ╟─8a87380d-20e6-4c27-950b-25003b98bf30
 # ╠═30c7d656-e197-495e-bd71-9c71214d1857
 # ╠═f4e3b67b-ee51-4648-b780-c563557e7ca6
-# ╠═59a442ee-3b6c-464b-8948-dbfe8e159774
+# ╟─59a442ee-3b6c-464b-8948-dbfe8e159774
 # ╠═fdbc2d16-b26a-4df2-b6e9-a6fa1a825be2
-# ╠═3b8df1df-1054-417a-b242-4046c99f2d02
+# ╟─3b8df1df-1054-417a-b242-4046c99f2d02
 # ╠═c2640ab8-8926-4872-abdf-adc509f56d59
-# ╠═e07cb9fb-f59c-43f1-bcb1-fedc0e1d90f9
+# ╟─e07cb9fb-f59c-43f1-bcb1-fedc0e1d90f9
 # ╠═c3541e3a-a376-4223-95b5-940b53adbba2
-# ╠═15924d1b-07b9-48b7-bc0f-b2836f391a1d
+# ╟─15924d1b-07b9-48b7-bc0f-b2836f391a1d
 # ╠═2758f368-ea18-473d-bfa4-4d65a3c94653
 # ╠═42b0eb6d-21a2-4b88-b09a-42bfc0289c51
-# ╠═befc5723-14b7-4047-ba88-c89bd4815a62
+# ╟─befc5723-14b7-4047-ba88-c89bd4815a62
 # ╠═49808249-8a31-475b-9a04-2630545bf4d4
-# ╠═6ba520fe-7928-4d4c-8b0c-705af8269739
+# ╟─6ba520fe-7928-4d4c-8b0c-705af8269739
 # ╠═dd958e98-aab7-489c-a6fc-6261d85fbea4
-# ╠═58bd5959-c4c9-4351-8852-063743b7a937
-# ╠═b2eb9a0d-cb44-492f-9297-26833ba41354
+# ╟─58bd5959-c4c9-4351-8852-063743b7a937
 # ╠═18966e29-62c4-4e0e-8b96-47ac0e026dc2
-# ╠═77b9a080-7a3b-4835-9d69-afa13eff3757
+# ╟─77b9a080-7a3b-4835-9d69-afa13eff3757
 # ╠═9c02ef80-80ca-4b33-84f6-8cba62527539
+# ╟─90d20692-ce49-419e-8377-9e2b72d176a6
+# ╠═3215092e-a913-4c73-ac01-fc725beccb05
 # ╟─00000000-0000-0000-0000-000000000001
 # ╟─00000000-0000-0000-0000-000000000002
